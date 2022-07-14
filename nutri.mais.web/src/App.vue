313c7ab3f4a27f1f1d7961a0b7fe8f62afd625b8
AlineGarcia <alinecarinagarcia@hotmail.com>
2022-07-14 18:04:03 -0300

    Cabecalho finalizado

---
+++ nutri.mais.web/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-app>
+    <Cabecalho/>
+  </v-app>
+</template>
+
+<script>
+import Cabecalho from './components/commons/Cabecalho.vue'
+export default {
+  name: 'App',
+  components: {
+    Cabecalho
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ nutri.mais.web/src/components/commons/Cabecalho.vue
@@ -0,0 +1,70 @@
+<template>
+    <header class="cabecalho">
+        <div class="logo">
+            <img src="../../assets/logo.png" alt="">
+            <strong class="nomelogo" >Nutri Mais</strong>
+        </div>
+        <div class="espacador"></div>
+        <div class="botao">
+            <button>login</button>
+            <button>Cadastre-se</button>
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'CabecalhoView',
+    components: {
+    }
+}
+</script>
+
+<style>
+    .cabecalho {
+        height: 60px;
+        background-color: rgba(255, 251, 241, 0.767);
+        position: fixed;
+        top: 0;
+        left: 0;
+        right: 0;
+        box-shadow: 0px 1px 4px 0px black;
+        display: flex;
+    }
+
+    .botao {
+        float: right;
+        height: 100%;
+        display: flex;
+    }
+
+    .botao button {
+        border: none;
+        font-size: 18px;
+        font-family: none;
+        background-color: transparent;
+        cursor: pointer;
+        margin: 0 10px;
+    }
+
+    .botao button:hover {
+        font-weight: 600;
+    }
+ 
+    .logo {
+        height: 100%;
+        display: flex;
+    }
+
+    .espacador {
+        margin-inline-start: auto;
+    }
+
+    .nomelogo {
+        font-size: 24px;
+        margin: auto;
+        margin-left: 15px;
+    }
+
+
+</style>
